4a6e3eb6f0fb92971abd12fa938a0184174751ce
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-11 01:01:49 +0200

    Create Project

---
+++ src/main/java/me/dev/motospring/MotospringApplication.java
@@ -0,0 +1,13 @@
+package me.dev.motospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dev/motospring/MotospringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dev.motospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
