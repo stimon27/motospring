1564b3bbd568935816ca5369a008a182afe51fa6
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-17 20:52:57 +0200

    Add Racing Teams List and update controller. Closes #15.

--- motospring-web/src/main/java/me/dev/motospring/controllers/RacingTeamController.java
+++ motospring-web/src/main/java/me/dev/motospring/controllers/RacingTeamController.java
@@ -1,14 +1,22 @@
 package me.dev.motospring.controllers;
 
+import me.dev.motospring.services.RacingTeamService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/racingteams")
 public class RacingTeamController {
+    private final RacingTeamService racingTeamService;
+
+    public RacingTeamController(RacingTeamService racingTeamService) {
+        this.racingTeamService = racingTeamService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listRacingTeams() {
+    public String listRacingTeams(Model model) {
+        model.addAttribute("racingteams", racingTeamService.findAll());
         return "racingTeams/index";
     }
 }
