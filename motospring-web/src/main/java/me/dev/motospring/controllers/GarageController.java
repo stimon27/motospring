811a60cbaf0258332792c5a7da6881a961f53bd3
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-17 20:56:50 +0200

    Add Garages List and update controller. Closes #21.

--- motospring-web/src/main/java/me/dev/motospring/controllers/GarageController.java
+++ motospring-web/src/main/java/me/dev/motospring/controllers/GarageController.java
@@ -1,14 +1,23 @@
 package me.dev.motospring.controllers;
 
+import me.dev.motospring.services.GarageService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/garages")
 public class GarageController {
+    private final GarageService garageService;
+
+    public GarageController(GarageService garageService) {
+        this.garageService = garageService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    String listGarages() {
+    String listGarages(Model model) {
+        model.addAttribute("garages", garageService.findAll());
+
         return "garages/index";
     }
 }
