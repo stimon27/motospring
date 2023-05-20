6e28084003ffc79eca45bdd7086feff50325b89c
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-20 19:25:32 +0200

    Add Make Service. Closes #29.

---
+++ motospring-data/src/main/java/me/dev/motospring/services/MakeService.java
@@ -0,0 +1,6 @@
+package me.dev.motospring.services;
+
+import me.dev.motospring.model.Make;
+
+public interface MakeService extends CrudService<Make, Long> {
+}
---
+++ motospring-data/src/main/java/me/dev/motospring/services/map/MakeMapService.java
@@ -0,0 +1,35 @@
+package me.dev.motospring.services.map;
+
+import me.dev.motospring.model.Make;
+import me.dev.motospring.services.MakeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class MakeMapService extends AbstractMapService<Make, Long> implements MakeService {
+    @Override
+    public Set<Make> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Make findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Make save(Make make) {
+        return super.save(make);
+    }
+
+    @Override
+    public void delete(Make make) {
+        super.delete(make);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
