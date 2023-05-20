7a77c3890fa7a441deaa3dd753f64d834b7f9230
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-21 01:26:27 +0200

    Create Spring Data Jpa Service for Makes. Closes #42.

---
+++ motospring-data/src/main/java/me/dev/motospring/services/springdatajpa/MakeJpaService.java
@@ -0,0 +1,48 @@
+package me.dev.motospring.services.springdatajpa;
+
+import me.dev.motospring.model.Make;
+import me.dev.motospring.repositories.MakeRepository;
+import me.dev.motospring.services.MakeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class MakeJpaService implements MakeService {
+
+    private final MakeRepository makeRepository;
+
+    public MakeJpaService(MakeRepository makeRepository) {
+        this.makeRepository = makeRepository;
+    }
+
+    @Override
+    public Set<Make> findAll() {
+        Set<Make> makes = new HashSet<>();
+        makeRepository.findAll().forEach(makes::add);
+        return makes;
+    }
+
+    @Override
+    public Make findById(Long id) {
+        return makeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Make save(Make make) {
+        return makeRepository.save(make);
+    }
+
+    @Override
+    public void delete(Make make) {
+        makeRepository.delete(make);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        makeRepository.deleteById(id);
+    }
+}
