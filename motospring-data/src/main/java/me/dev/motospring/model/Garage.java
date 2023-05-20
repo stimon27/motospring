ccbba61e3e41ee2287fb99eea10734f8409c4fde
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-21 00:35:14 +0200

    Convert Garages to JPA Entities. Closes #37.

--- motospring-data/src/main/java/me/dev/motospring/model/Garage.java
+++ motospring-data/src/main/java/me/dev/motospring/model/Garage.java
@@ -1,10 +1,17 @@
 package me.dev.motospring.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "garages")
 public class Garage extends Group {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "garage_style",
+            joinColumns = @JoinColumn(name = "garage_id"),
+            inverseJoinColumns = @JoinColumn(name = "style_id"))
     private Set<Style> styles = new HashSet<>();
 
     public Set<Style> getStyles() {
--- motospring-data/src/main/java/me/dev/motospring/model/Style.java
+++ motospring-data/src/main/java/me/dev/motospring/model/Style.java
@@ -1,7 +1,14 @@
 package me.dev.motospring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "styles")
 public class Style extends BaseEntity {
 
+    @Column(name = "tag")
     private String tag;
 
     public String getTag() {
