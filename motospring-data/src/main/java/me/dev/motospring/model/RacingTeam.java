6b3725845a6573d061195c01a084ad1643345951
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-20 19:15:01 +0200

    Add Racing Team Detail Info. Closes #28.

--- motospring-data/src/main/java/me/dev/motospring/model/RacingTeam.java
+++ motospring-data/src/main/java/me/dev/motospring/model/RacingTeam.java
@@ -1,10 +1,38 @@
 package me.dev.motospring.model;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 public class RacingTeam extends Group {
 
+    private String racingDiscipline;
+    private LocalDate creationDate;
+    private String motto;
     private Set<Car> cars;
+
+    public String getRacingDiscipline() {
+        return racingDiscipline;
+    }
+
+    public void setRacingDiscipline(String racingDiscipline) {
+        this.racingDiscipline = racingDiscipline;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getMotto() {
+        return motto;
+    }
+
+    public void setMotto(String motto) {
+        this.motto = motto;
+    }
 
     public Set<Car> getCars() {
         return cars;
