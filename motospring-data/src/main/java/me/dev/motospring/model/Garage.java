ad46a27f5f8a50f10665f5eaf7b0fa243ea2c89e
stimon27 <86240169+stimon27@users.noreply.github.com>
2023-05-20 19:05:38 +0200

    Add Style, Add it to Garage. Closes #27.

--- motospring-data/src/main/java/me/dev/motospring/model/Garage.java
+++ motospring-data/src/main/java/me/dev/motospring/model/Garage.java
@@ -1,4 +1,16 @@
 package me.dev.motospring.model;
 
+import java.util.Set;
+
 public class Garage extends Group {
+
+    private Set<Style> styles;
+
+    public Set<Style> getStyles() {
+        return styles;
+    }
+
+    public void setStyles(Set<Style> styles) {
+        this.styles = styles;
+    }
 }
---
+++ motospring-data/src/main/java/me/dev/motospring/model/Style.java
@@ -0,0 +1,14 @@
+package me.dev.motospring.model;
+
+public class Style extends BaseEntity {
+
+    private String tag;
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
